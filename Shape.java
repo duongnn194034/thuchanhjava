517cf28aedccb0e2cffdd5f3d561a4db2e43220c
duongnn194034 <67011150+duongnn194034@users.noreply.github.com>
2022-03-18 17:26:27 +0700

    Add files via upload

---
+++ Shape.java
@@ -0,0 +1,16 @@
+package baithuchanh4;
+
+public abstract class Shape {
+    public void Operation () {
+
+    }
+    public void add (Shape shape) {
+
+    }
+    public void remove (Shape shape) {
+
+    }
+    public void GetChild (int i) {
+
+    }
+}
