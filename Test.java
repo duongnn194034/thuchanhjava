87c2c22d065017941e9b522bcdf7a9e646c408bd
duongnn194034 <67011150+duongnn194034@users.noreply.github.com>
2022-03-21 11:03:29 +0700

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,27 @@
+package baithuchanh10;
+
+//import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FilterInputStream;
+import java.io.IOException;
+
+public class Test {
+    public static void main(String args[]) throws IOException {
+        FileInputStream file = null;
+        FilterInputStream filter = null;
+        try {
+            file = new FileInputStream(new File("D:\\testout.txt"));
+            filter = new MyFilterInputStream(file);
+ 
+            int k = 0;
+            while ((k = filter.read()) != -1) {
+                System.out.print((char) k);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        file.close();
+        filter.close();
+    }
+}
